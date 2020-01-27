931b3e1f6059ce07acad4b3875fd11ca5d033eeb
Pyrolisfer <44378813+Pyrolisfer@users.noreply.github.com>
2020-01-27 16:05:09 +0000

    Add files via upload

---
+++ boxPuzzleFinish.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxPuzzleFinish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int puzzleCompletion = 0;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(puzzleCompletion);
+
+        if(puzzleCompletion == 3)
+        {
+            Debug.Log("puzzlecomplete");
+        }
+    }
+   
+}
+
+
