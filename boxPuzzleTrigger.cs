91d6ef7455ed3c41b957f2cd70f2111ac002369e
Pyrolisfer <44378813+Pyrolisfer@users.noreply.github.com>
2020-01-27 16:03:55 +0000

    Add files via upload

---
+++ boxPuzzleTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxPuzzleTrigger : MonoBehaviour
+{
+    public int boxPuzzle = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider box)
+    {
+
+        if (box.isTrigger == true && box.CompareTag("boxPuzzleTrigger"))
+        {
+            
+            GameObject completePuzzle = GameObject.Find("test");
+            boxPuzzleFinish testing = completePuzzle.GetComponent<boxPuzzleFinish>();
+            testing.puzzleCompletion += 1;
+            
+        }
+
+
+
+
+    }
+    void OnTriggerExit(Collider box)
+    {
+
+        if (box.isTrigger == true && box.CompareTag("boxPuzzleTrigger"))
+        {
+            
+            GameObject completePuzzle = GameObject.Find("test");
+            boxPuzzleFinish testing = completePuzzle.GetComponent<boxPuzzleFinish>();
+            testing.puzzleCompletion -= 1;
+
+        }
+
+
+
+
+    }
+}
+
+
+
